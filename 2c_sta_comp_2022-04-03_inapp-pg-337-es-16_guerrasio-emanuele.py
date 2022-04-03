09c220c1af825e6908f801cacf670f51e039971f
BlueShot1 <73930392+BlueShot1@users.noreply.github.com>
2022-04-03 11:59:13 +0200

    Add files via upload

---
+++ 2c_sta_comp_2022-04-03_inapp-pg-337-es-16_guerrasio-emanuele.py
@@ -0,0 +1,24 @@
+import random
+vet = []
+vet2 = []
+lenVet = 15
+min = 2
+max = 20
+pivot = random.randint(2, 15)
+
+for i in range(lenVet)
+    vet.append(random.randint(min, max))
+
+for i in range(lenVet)
+    if vet[i]  vet[pivot]
+        vet2.append(vet[i])
+
+vet2.append(vet[pivot])
+
+for i in range(lenVet)
+    if vet[i]  vet[pivot]
+        vet2.append(vet[i])
+
+print(vet)
+print(posizione pivot , pivot + 1,  con valore , vet[pivot])
+print(vet2)
